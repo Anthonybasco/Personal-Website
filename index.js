c4534b65f2d08041a2bce7b35a8a544c297a68b9
Anthonybasco <abasco2@outlook.com>
2024-09-23 14:43:44 -0500

    added contacts

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("This is my first Javascript Program");
+
+const MyName = "Anthony Basco";
+console.log(MyName);
+
+const Message =
+  " You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 40 39";
+console.log(Message);
+
+//This is the message you will receive
+let str1 =
+  " Success! your email and password are saved!";
+
+alert(str1);
